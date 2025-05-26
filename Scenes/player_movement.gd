d7c6980e8dfd145182d01580751fbc7039d78401
chocoboash <anleal2004@gmail.com>
2025-05-25 21:22:49 -0400

    Sprite animations and movement script

--- Scenes/player_movement.gd
+++ Scenes/player_movement.gd
@@ -1,19 +1,39 @@
 extends CharacterBody2D
 
-@export var movement_speed : float = 500
-var character_direction : Vector2
+@export var movement_speed: float = 500
+@onready var _animated_sprite = %sprite
+var character_direction: Vector2
+var last_direction: Vector2 = Vector2.DOWN  # default facing down
 
 func _physics_process(delta):
 	character_direction.x = Input.get_axis("move_left", "move_right")
 	character_direction.y = Input.get_axis("move_up", "move_down")
 	character_direction = character_direction.normalized()
 	
-	if character_direction.x > 0: %sprite.flip_h = false
-	elif character_direction.x < 0: %sprite.flip_h = true
-	
-	if character_direction: 
+	if character_direction != Vector2.ZERO:
+		last_direction = character_direction
 		velocity = character_direction * movement_speed
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
 
 	move_and_slide()
+
+	# Animation logic
+	if character_direction != Vector2.ZERO:
+		if abs(character_direction.y) > abs(character_direction.x):
+			if character_direction.y > 0:
+				_animated_sprite.play("run_down")
+			else:
+				_animated_sprite.play("run_up")
+		else:
+			_animated_sprite.play("run_side")
+			_animated_sprite.flip_h = character_direction.x > 0
+	else:
+		if abs(last_direction.y) > abs(last_direction.x):
+			if last_direction.y > 0:
+				_animated_sprite.play("idle_front")
+			else:
+				_animated_sprite.play("idle_back")
+		else:
+			_animated_sprite.play("idle_side")
+			_animated_sprite.flip_h = last_direction.x > 0
