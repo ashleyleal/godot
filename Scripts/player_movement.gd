d061e0ac823799f6fd8bf42535322e164f352bf4
chocoboash <anleal2004@gmail.com>
2025-05-27 19:32:35 -0400

    Refactor animation controller into FSM

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -2,8 +2,20 @@ extends CharacterBody2D
 
 @export var movement_speed: float = 75
 @onready var _animated_sprite = %sprite
+
 var character_direction: Vector2
 var last_direction: Vector2 = Vector2.DOWN 
+
+enum AnimationState {
+	IDLE_FRONT,
+	IDLE_BACK,
+	IDLE_SIDE,
+	RUN_DOWN,
+	RUN_UP,
+	RUN_SIDE
+}
+
+var current_state: AnimationState = AnimationState.IDLE_FRONT
 
 func _physics_process(delta):
 	character_direction.x = Input.get_axis("move_left", "move_right")
@@ -23,25 +35,38 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, current_speed)
 
 	move_and_slide()
-
 	_animated_sprite.speed_scale = speed_multiplier
 
-	# Animation logic
+	update_animation_state()
+	play_animation()
+
+
+func update_animation_state():
 	if character_direction != Vector2.ZERO:
 		if abs(character_direction.y) > abs(character_direction.x):
-			if character_direction.y > 0:
-				_animated_sprite.play("run_down")
-			else:
-				_animated_sprite.play("run_up")
+			current_state = AnimationState.RUN_DOWN if character_direction.y > 0 else AnimationState.RUN_UP
 		else:
-			_animated_sprite.play("run_side")
-			_animated_sprite.flip_h = character_direction.x > 0
+			current_state = AnimationState.RUN_SIDE
 	else:
 		if abs(last_direction.y) > abs(last_direction.x):
-			if last_direction.y > 0:
-				_animated_sprite.play("idle_front")
-			else:
-				_animated_sprite.play("idle_back")
+			current_state = AnimationState.IDLE_FRONT if last_direction.y > 0 else AnimationState.IDLE_BACK
 		else:
+			current_state = AnimationState.IDLE_SIDE
+
+
+func play_animation():
+	match current_state:
+		AnimationState.RUN_DOWN:
+			_animated_sprite.play("run_down")
+		AnimationState.RUN_UP:
+			_animated_sprite.play("run_up")
+		AnimationState.RUN_SIDE:
+			_animated_sprite.play("run_side")
+			_animated_sprite.flip_h = character_direction.x > 0
+		AnimationState.IDLE_FRONT:
+			_animated_sprite.play("idle_front")
+		AnimationState.IDLE_BACK:
+			_animated_sprite.play("idle_back")
+		AnimationState.IDLE_SIDE:
 			_animated_sprite.play("idle_side")
 			_animated_sprite.flip_h = last_direction.x > 0
