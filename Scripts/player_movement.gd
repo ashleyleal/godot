dd19926826f95d770142f9b58cf2abacf8493aa7
chocoboash <anleal2004@gmail.com>
2025-05-26 20:20:57 -0400

    Janky ahh tilemap design and sprite sprint

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -1,22 +1,30 @@
 extends CharacterBody2D
 
-@export var movement_speed: float = 500
+@export var movement_speed: float = 75
 @onready var _animated_sprite = %sprite
 var character_direction: Vector2
-var last_direction: Vector2 = Vector2.DOWN  # default facing down
+var last_direction: Vector2 = Vector2.DOWN 
 
 func _physics_process(delta):
 	character_direction.x = Input.get_axis("move_left", "move_right")
 	character_direction.y = Input.get_axis("move_up", "move_down")
 	character_direction = character_direction.normalized()
-	
+
+	var speed_multiplier = 1.0
+	if Input.is_action_pressed("sprint"):
+		speed_multiplier = 1.5
+
+	var current_speed = movement_speed * speed_multiplier
+
 	if character_direction != Vector2.ZERO:
 		last_direction = character_direction
-		velocity = character_direction * movement_speed
+		velocity = character_direction * current_speed
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
+		velocity = velocity.move_toward(Vector2.ZERO, current_speed)
 
 	move_and_slide()
+
+	_animated_sprite.speed_scale = speed_multiplier
 
 	# Animation logic
 	if character_direction != Vector2.ZERO:
